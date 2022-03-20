6a3179cd62a6e2b473f5f0ef764fda83bd4b7674
du than <du.than@relexsolutions.com>
2022-03-20 18:33:47 +0200

    saved

--- front-end/src/app/profile/profile.component.ts
+++ front-end/src/app/profile/profile.component.ts
@@ -1,4 +1,6 @@
 import { Component } from "@angular/core";
+import { FormControl, FormGroup } from "@angular/forms";
+import { MatChipInputEvent } from "@angular/material/chips";
 
 @Component({
   selector: 'app-profile',
@@ -9,43 +11,54 @@ export class ProfileComponent {
   constructor() {
   }
 
-}
+  userProfileForm: FormGroup = new FormGroup({
+    gender: new FormControl(),
+    sexualOrientation: new FormControl(),
+    biography: new FormControl(),
+    listOfInterest: new FormControl(),
+    picture: new FormControl()
+  });
 
-const questionList = [
-  {
-    id: 1,
-    questionText: 'What is your estimated skill level',
-    options: [
-      'Beginer',
-      'Experienced',
-      'Advanced'
-    ]
-  },
-  {
-    id: 2,
-    questionText: 'Sth2',
-    options: [
-      'a',
-      'b',
-      'c'
-    ]
-  },
-  {
-    id: 3,
-    questionText: 'Sth3',
-    options: [
-      'a',
-      'b',
-      'c'
-    ]
-  },
-  {
-    id: 4,
-    questionText: 'Sth4',
-    options: [
-      'a',
-      'b',
-      'c'
-    ]
+  onSubmit(): void {
+    console.log(this.userProfileForm);
   }
-]
+
+
+  firstFormGroup: FormGroup = new FormGroup({
+    gender: new FormControl()
+  });
+
+  secondFormGroup: FormGroup = new FormGroup({
+    sexualOrientation: new FormControl()
+  });
+
+  thirdFormGroup: FormGroup = new FormGroup({
+    biography: new FormControl()
+  });
+
+  forthFormGroup: FormGroup = new FormGroup({
+    interest: new FormControl()
+  });
+
+  interests = [
+    { name: 'cat' }, { name: 'dog' }, { name: 'fish' }
+  ];
+
+  onRemove(item: any): void {
+    const index = this.interests.indexOf(item);
+
+    if (index >= 0) {
+      this.interests.splice(index, 1);
+    }
+  }
+
+  onAdd(event: MatChipInputEvent): void {
+    const value = (event.value || '').trim();
+
+    if (value) {
+      this.interests.push({name: value});
+    }
+
+    event.chipInput!.clear();
+  }
+}
--- front-end/src/app/profile/profile.module.ts
+++ front-end/src/app/profile/profile.module.ts
@@ -1,7 +1,17 @@
 import { CommonModule } from "@angular/common";
 import { NgModule } from "@angular/core";
+import { ReactiveFormsModule } from "@angular/forms";
+import { MatButtonModule } from "@angular/material/button";
+import { MatFormFieldModule } from "@angular/material/form-field";
+import { MatInputModule } from "@angular/material/input";
+import { MatTooltipModule } from "@angular/material/tooltip";
 import { ProfileRoutingModule } from "./profile-routing.module";
 import { ProfileComponent } from "./profile.component";
+import { MatRadioModule } from '@angular/material/radio';
+import {MatStepperModule} from '@angular/material/stepper';
+import {MatChipsModule} from '@angular/material/chips';
+import { MatIconModule } from "@angular/material/icon";
+import {MatButtonToggleModule} from '@angular/material/button-toggle';
 
 @NgModule({
   declarations: [
@@ -9,7 +19,17 @@ import { ProfileComponent } from "./profile.component";
   ],
   imports: [
     CommonModule,
-    ProfileRoutingModule
+    ReactiveFormsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule,
+    MatTooltipModule,
+    MatRadioModule,
+    MatStepperModule,
+    MatChipsModule,
+    MatIconModule,
+    ProfileRoutingModule,
+    MatButtonToggleModule
   ],
 })
 export class ProfileModule {}
